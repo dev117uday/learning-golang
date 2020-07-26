a493f307a142481fbdef49ac704a5e1c811f5d16
Uday Yadav <dev117uday@gmail.com>
2020-07-26 18:25:12 +0530

    refactor

--- IDE/src/main.go
+++ IDE/src/main.go
@@ -1,23 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	var vector []int64
-	{
-		reader := bufio.NewReader(os.Stdin)
-		integerString,_ := reader.ReadString('\n')
-		tempArray := strings.Split(integerString," ")
-		for i:=0; i<len(tempArray); i++ {
-			temp,_ := strconv.ParseInt(strings.TrimSpace(tempArray[i]),10,64)
-			vector = append(vector,temp)
-		}
-	}
-	fmt.Println("\nVector : ",vector)
+	var vector2d [0][0]int32
+	fmt.Println(len(vector2d))
 }
