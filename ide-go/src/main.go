c88961cda4acd675949263d61fe3ff2e82a164eb
Uday Yadav <dev117uday@gmail.com>
2020-08-30 14:25:18 +0530

    backup

--- ide-go/src/main.go
+++ ide-go/src/main.go
@@ -1,26 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
+import "fmt"
 
 func main() {
 
-	var number int
-	_, _ = fmt.Scan(&number)
-	var oned = make([]int,number)
-	for i:=0; i<len(oned); i++ {
-		_, _ = fmt.Scan(&oned[i])
-	}
-	sort.Ints(oned)
-	counter := 0
-	for i:=0; i<len(oned); i++ {
-		for j:=0; j<len(oned); j++ {
-			
-		}
-	}
-
-	fmt.Println(counter)
+	fmt.Println("Hello World")
 
 }
